2760ed848a6bda6ee0212a4c312083977736e6c1
Shino1024 <justincompspec@gmail.com>
2017-11-10 15:58:18 +0100

    [Backup] Works on the splash screen and its activity.

--- app/src/main/java/net/linuxutopia/studenteat/SplashActivity.java
+++ app/src/main/java/net/linuxutopia/studenteat/SplashActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by jaros on 11/5/2017.
@@ -12,13 +16,18 @@ import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Animation titleAnimation;
+    ImageView logo;
+    TextView title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.splash_screen);
+
+        titleAnimation = AnimationUtils.loadAnimation(this, R.anim.title_animation);
+        title = findViewById(R.id.title);
+        title.startAnimation(titleAnimation);
     }
 
 }
