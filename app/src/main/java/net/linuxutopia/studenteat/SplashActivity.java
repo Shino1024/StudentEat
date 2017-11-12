37c788b115f7042eea6fb3de57922526543aa6f9
Shino1024 <justincompspec@gmail.com>
2017-11-12 17:22:41 +0100

    Updated SplashScreen and created a new animation. Also, prepared for the activity transition.

--- app/src/main/java/net/linuxutopia/studenteat/SplashActivity.java
+++ app/src/main/java/net/linuxutopia/studenteat/SplashActivity.java
@@ -1,15 +1,21 @@
 package net.linuxutopia.studenteat;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by jaros on 11/5/2017.
@@ -18,19 +24,52 @@ import android.widget.TextView;
 public class SplashActivity extends AppCompatActivity {
 
     Animation titleAnimation;
-    TextView title;
+    TextView splashTitle;
     Typeface custom_font;
+    DisplayMetrics displayMetrics = new DisplayMetrics();
+    int screenWidth;
+    Handler splashEndHandler = new Handler();
+    Intent mainActivityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenWidth = displayMetrics.widthPixels;
+
         custom_font = Typeface.createFromAsset(getAssets(),"atma.ttf");
+
+        mainActivityIntent = new Intent(this, MainActivity.class);
+
         titleAnimation = AnimationUtils.loadAnimation(this, R.anim.title_animation);
-        title = findViewById(R.id.splashTitle);
-        title.setTypeface(custom_font);
-        title.startAnimation(titleAnimation);
+        titleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                splashEndHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(mainActivityIntent);
+                    }
+                }, 500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        splashTitle = findViewById(R.id.splashTitle);
+        splashTitle.setTypeface(custom_font);
+        splashTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, 0.10f * screenWidth);
+        splashTitle.startAnimation(titleAnimation);
+
     }
 
 }
